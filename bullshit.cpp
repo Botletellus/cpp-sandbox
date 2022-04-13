9236cc532adf0996b149084291cb4858f63901b0
Botletellus <boletellus@protonmail.ch>
2022-04-13 22:49:24 +0200

    add of the bullshit.cpp file

---
+++ bullshit.cpp
@@ -0,0 +1,9 @@
+#include "iostream"
+#include "vector"
+
+int main(){
+    std::cout<<"Entrer un nombre entier random: " << "\n";
+    int userRandom;
+    std::cin >> userRandom;
+    std::cout << "Votre nombre est : " << userRandom << "\n";
+}

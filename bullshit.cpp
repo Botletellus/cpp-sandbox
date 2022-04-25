692cff57b2d6daf61264fe2387d6621d8febfccc
Botletellus <boletellus@protonmail.ch>
2022-04-25 17:56:06 +0200

    teste

--- bullshit.cpp
+++ bullshit.cpp
@@ -1,9 +1,17 @@
 #include "iostream"
 #include "vector"
 
-int main(){
-    std::cout<<"Entrer un nombre entier random: " << "\n";
+int main() {
+    std::cout << "Enter a random integer between 0 and 100: " << "\n";
+    bool test = false;
     int userRandom;
-    std::cin >> userRandom;
-    std::cout << "Votre nombre est : " << userRandom << "\n";
+    while (!test) {
+        std::cin >> userRandom;
+        if (userRandom <= 0 || userRandom >= 100) {
+            std::cout << "the number must be between 0 and 100" << "\n";
+        } else {
+            test = true;
+        }
+    }
+    std::cout << "Your number is : " << userRandom << "\n";
 }
